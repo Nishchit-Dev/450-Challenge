6f0cc170c7cbe926d9c71407e18cdd9bcafa64e0
Nishchit-dev <nishchitpatel84@gmail.com>
2023-11-14 11:33:37 -0600

    introduced MinHeap Class

--- Data_Strucutre/heap/heap.cpp
+++ Data_Strucutre/heap/heap.cpp
@@ -3,103 +3,195 @@
 #include <vector>
 using namespace std;
 
-class heap
+class MaxHeap
 {
+public:
+    void swap(int *a, int *b)
+    {
+        int temp = *b;
+        *b = *a;
+        *a = temp;
+    }
+
+    void heapify(vector<int> &v, int NodeIndex)
+    {
+
+        int size = v.size();
+        int MaximunChildIndex = NodeIndex;
+        int LeftChild = 2 * NodeIndex + 1;
+        int RightChild = 2 * NodeIndex + 2;
+        if (LeftChild < size && v[MaximunChildIndex] < v[LeftChild])
+        {
+            MaximunChildIndex = LeftChild;
+        }
+
+        if (RightChild < size && v[MaximunChildIndex] < v[RightChild])
+        {
+            MaximunChildIndex = RightChild;
+        }
+
+        if (MaximunChildIndex != NodeIndex)
+        {
+            swap(&v[MaximunChildIndex], &v[NodeIndex]);
+            heapify(v, MaximunChildIndex);
+        }
+    }
+
+    void deleteHeap(vector<int> &v, int num)
+    {
+        // first we find the number get the index of that number and we replace that number with last element and we apply heapify
+        int index = 0;
+        int size = v.size();
+
+        for (int i = 0; i < size; i++)
+        {
+            if (v[i] == num)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // swap the element
+        swap(&v[index], &v[size - 1]);
+        v.pop_back();
+        // then heapify
+
+        for (int j = (size / 2) - 1; j >= 0; j--)
+        {
+            // heapify
+            heapify(v, j);
+        }
+    }
+    void insert(vector<int> &v, int data)
+    {
+        int size = v.size();
+        // 1st checkpoint
+        if (size == 0)
+        {
+            v.push_back(data);
+        }
+        else
+        {
+            v.push_back(data);
+            // now we do heapify
+            for (int i = size / 2; i >= 0; i--)
+            {
+                heapify(v, i);
+            }
+        }
+    }
+
+    void Display(vector<int> &v)
+    {
+        for (auto ele : v)
+        {
+            cout << ele << " ";
+        }
+    }
 };
-void swap(int *a, int *b)
-{
-    int temp = *b;
-    *b = *a;
-    *a = temp;
-}
 
-void heapify(vector<int> &v, int NodeIndex)
+class MinHeap
 {
-
-    int size = v.size();
-    int MaximunChildIndex = NodeIndex;
-    int LeftChild = 2 * NodeIndex + 1;
-    int RightChild = 2 * NodeIndex + 2;
-    if (LeftChild < size && v[MaximunChildIndex] < v[LeftChild])
+public:
+    void swap(int *a, int *b)
     {
-        MaximunChildIndex = LeftChild;
+        int temp = *b;
+        *b = *a;
+        *a = temp;
     }
 
-    if (RightChild < size && v[MaximunChildIndex] < v[RightChild])
+    void heapify(vector<int> &v, int NodeIndex)
     {
-        MaximunChildIndex = RightChild;
-    }
 
-    if (MaximunChildIndex != NodeIndex)
-    {
-        swap(&v[MaximunChildIndex], &v[NodeIndex]);
-        heapify(v, MaximunChildIndex);
-    }
-}
-
-void deleteHeap(vector<int> &v, int num)
-{
-    // first we find the number get the index of that number and we replace that number with last element and we apply heapify
-    int index = 0;
-    int size = v.size();
-
-    for (int i = 0; i < size; i++)
-    {
-        if (v[i] == num)
+        int size = v.size();
+        int MaximunChildIndex = NodeIndex;
+        int LeftChild = 2 * NodeIndex + 1;
+        int RightChild = 2 * NodeIndex + 2;
+        if (LeftChild < size && v[MaximunChildIndex] > v[LeftChild])
         {
-            index = i;
-            break;
+            MaximunChildIndex = LeftChild;
+        }
+
+        if (RightChild < size && v[MaximunChildIndex] > v[RightChild])
+        {
+            MaximunChildIndex = RightChild;
+        }
+
+        if (MaximunChildIndex != NodeIndex)
+        {
+            swap(&v[MaximunChildIndex], &v[NodeIndex]);
+            heapify(v, MaximunChildIndex);
         }
     }
 
-    // swap the element
-    swap(&v[index], &v[size - 1]);
-    v.pop_back();
-    // then heapify
+    void deleteHeap(vector<int> &v, int num)
+    {
+        // first we find the number get the index of that number and we replace that number with last element and we apply heapify
+        int index = 0;
+        int size = v.size();
 
-    for (int j = (size / 2) - 1; j >= 0; j--)
-    {
-        // heapify
-        heapify(v, j);
-    }
-}
-void insert(vector<int> &v, int data)
-{
-    int size = v.size();
-    // 1st checkpoint
-    if (size == 0)
-    {
-        v.push_back(data);
-    }
-    else
-    {
-        v.push_back(data);
-        // now we do heapify
-        for (int i = size / 2; i >= 0; i--)
+        for (int i = 0; i < size; i++)
         {
-            heapify(v, i);
+            if (v[i] == num)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // swap the element
+        swap(&v[index], &v[size - 1]);
+        v.pop_back();
+        // then heapify
+
+        for (int j = (size / 2) - 1; j >= 0; j--)
+        {
+            // heapify
+            heapify(v, j);
         }
     }
-}
-
-void Display(vector<int> &v)
-{
-    for (auto ele : v)
+    void insert(vector<int> &v, int data)
     {
-        cout << ele << " ";
+        int size = v.size();
+        // 1st checkpoint
+        if (size == 0)
+        {
+            v.push_back(data);
+        }
+        else
+        {
+            v.push_back(data);
+            // now we do heapify
+            for (int i = size / 2; i >= 0; i--)
+            {
+                heapify(v, i);
+            }
+        }
     }
-}
+
+    void Display(vector<int> &v)
+    {
+        for (auto ele : v)
+        {
+            cout << ele << " ";
+        }
+    }
+};
 
 int main()
 {
-    vector<int> v;
-    insert(v, 1);
-    insert(v, 4);
-    insert(v, 2);
-    insert(v, 6);
-    insert(v, 3);
-    insert(v, 9);
-    insert(v, 10);
+    MaxHeap MaxH;
+    MinHeap MinH;
 
-    Display(v);
+    vector<int> v;
+    MaxH.insert(v, 1);
+    MaxH.insert(v, 4);
+    MaxH.insert(v, 2);
+    MaxH.insert(v, 6);
+    MaxH.insert(v, 30);
+    MaxH.insert(v, 9);
+    // Mh.insert(v, 10);
+
+    MaxH.Display(v);
 }
