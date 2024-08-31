d8eea39c981a841f68ddb414ad3c6d75abb29dd7
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-08-31 17:27:35 -0500

    solved palindromString

--- PalindromeString/palindromeString.cpp
+++ PalindromeString/palindromeString.cpp
@@ -1,31 +1,84 @@
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-bool checkStringPalindrome(string s,int i=0){
-
-    int iterator = s.length();
-    if(i >= iterator/2)
+string removeSpace(string &s)
+{
+    string newString = "";
+    int i = 0;
+    while (i < s.length())
     {
-        cout<<"palindrome";
-        return true;
-    }else{
-        if(s[i] != s[iterator - i - 1]){
-            cout<<"not palindrome";
-            return false;
-        }else{
-            cout<<"checking palindrome\n";
-            checkStringPalindrome(s,i+1);
+        // cout<< s[i];
+        if (isalpha(s[i]))
+        {
+            newString += tolower(s[i]);
         }
+        else if (isdigit(s[i]))
+        {
+            newString += tolower(s[i]);
+        }
+        i++;
     }
 
+    return newString;
 }
 
-int main(){
-    string str = "kek";
+bool checkStringPalindrome(string s, int i = 0)
+{
 
-    bool result = checkStringPalindrome(str);
+    int iterator = s.length();
+    if (i >= iterator / 2)
+    {
+        cout << "palindrome";
+        return true;
+    }
+    else
+    {
+        if (s[i] != s[iterator - i - 1])
+        {
+            cout << "not palindrome";
+            return false;
+        }
+        return checkStringPalindrome(s, i + 1);
+    }
+}
 
-    cout<<"\n"<< result;
+void removeSpaceOptmized(string &s)
+{
+    int j = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (isalpha(s[i]) || isdigit(s[i]))
+        {
+            s[j++] = tolower(s[i]);
+        }
+    }
+    s.resize(j); // Resize the string to remove extra characters
+}
+
+bool isPalindromeOptmized(string s)
+{
+    removeSpace(s);
+
+    int left = 0;
+    int right = s.length() - 1;
+    while (left < right)
+    {
+        if (s[left] != s[right])
+        {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
+}
+
+int main()
+{   
+    string str = "madam";
+
+    cout<< isPalindromeOptmized(str);
+
 }
