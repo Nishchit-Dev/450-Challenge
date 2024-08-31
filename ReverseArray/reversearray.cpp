c7091920945aa98305ae953ce093d9993573d232
Nishchit-Dev <nishchitpatel84@gmail.com>
2024-08-31 15:28:51 -0500

    structured reverseArray functions

--- ReverseArray/reversearray.cpp
+++ ReverseArray/reversearray.cpp
@@ -13,43 +13,39 @@ int swap(vector<int> &v, int a, int b)
 
 int reverseArray(vector<int> &x, int i)
 {
-    // if (x.size() % 2 == 0)
-    // {
-    //     if (i <= x.size() / 2 )
-    //     {
-    //         swap(x, x.size() - i, i - 1);
-    //         reverseArray(x, i + 1);
-    //     }
-    // }
-    // else
-    // {
-    //     if (i <= x.size() / 2 + 1)
-    //     {
-    //         swap(x, x.size() - i, i - 1);
-    //         reverseArray(x, i + 1);
-    //     }
-    // }
-
-    if(x.size() - i >= i  ){
-        swap(x,x.size()-i,i-1);
-        reverseArray(x,i+1);
+    if (x.size() % 2 == 0)
+    {
+        if (i <= x.size() / 2)
+        {
+            swap(x, x.size() - i, i - 1);
+            reverseArray(x, i + 1);
+        }
+    }
+    else
+    {
+        if (i <= x.size() / 2 + 1)
+        {
+            swap(x, x.size() - i, i - 1);
+            reverseArray(x, i + 1);
+        }
     }
 }
 
-int reverseArray_optimized(vector<int> &x, int i){
-    if(x.size() - i >= i  ){
-        swap(x,x.size()-i,i-1);
-        reverseArray(x,i+1);
+int reverseArray_optimized(vector<int> &x, int i)
+{
+    if (x.size() - i >= i)
+    {
+        swap(x, x.size() - i, i - 1);
+        reverseArray(x, i + 1);
     }
 }
 
 int main()
 {
-    vector<int> arr = {0 ,3,1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> arr = {0, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     // reverseArray(arr, 0);
     reverseArray_optimized(arr, 0);
-
 
     for (auto data : arr)
     {
